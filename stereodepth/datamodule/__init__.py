a1522c5451e34a5f3456a7b974b85ca8f0468f9d
BigDen <mangazeev.di@phystech.edu>
2024-02-13 23:58:57 +0300

    add: kitti2015 datamodule

---
+++ stereodepth/datamodule/__init__.py
@@ -0,0 +1,5 @@
+from stereodepth.datamodule.kitti2015 import KITTI2015DataModule
+
+__all__ = [
+    'KITTI2015DataModule',
+]
---
+++ stereodepth/datamodule/kitti2015.py
@@ -0,0 +1,191 @@
+import cv2
+import numpy as np
+from pathlib import Path
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from lightning import LightningDataModule
+from sklearn.model_selection import train_test_split
+
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+
+class KITTI2015Dataset(Dataset):
+    """
+    Pytorch dataset for the KITTI2015Dataset.
+
+    Attributes:
+        data_root (Path): Root dataset directory
+        file_list (list[str]): List of file names with suffix
+        transforms (A.Compose | None): Albumentations transformation, Defaults to `None`
+
+    Methods:
+        __init__: Initialize the `KITTI2015Dataset` object
+        __len__: Return number of files
+        __getitem__: Return left_img, reight_img and disp by index
+
+    """
+    def __init__(
+            self,
+            data_root: Path,
+            file_list: list[str],
+            transforms: A.Compose | None = None,
+    ):
+        super().__init__()
+        self.data_root = data_root
+        self.file_list = file_list
+        self.transforms = transforms
+
+    def __len__(self) -> int:
+        """
+        Returns:
+            number of files
+        """
+        return len(self.file_list)
+
+    def __getitem__(self, idx: int) -> dict[str, tuple[np.ndarray | torch.Tensor]]:
+        """
+        Takes left, right, disp by index, applies augmentations to them.
+
+        Args:
+            idx (int): Index of the file
+
+        Returns:
+            left (np.ndarray | torch.Tensor): augmented left image.
+            right (np.ndarray | torch.Tensor): augmented right image.
+            disp (np.ndarray | torch.Tensor): augmented disparity.
+        """
+        left_path = self.data_root / "image_2" / self.file_list[idx]
+        right_path = self.data_root / "image_3" / self.file_list[idx]
+        disp_path = self.data_root / "disp_occ_0" / self.file_list[idx]
+
+        left = cv2.imread(str(left_path))
+        right = cv2.imread(str(right_path))
+        disp = cv2.imread(str(disp_path), cv2.IMREAD_UNCHANGED).astype(np.float32) / 256
+
+        if self.transforms:
+            augment = self.transforms(image=left, right=right, disp=disp)
+            left, right, disp = augment['image'], augment['right'], augment['disp']
+
+        data = {
+            "left": left,
+            "right": right,
+            "disp": disp,
+        }
+
+        return data
+
+
+class KITTI2015DataModule(LightningDataModule):
+    """
+    """
+
+    def __init__(
+            self,
+            data_root: str,
+            train_transforms: A.Compose | None = None,
+            val_transforms: A.Compose | None = None,
+            train_val_split: tuple[float, float] = (0.9, 0.1),
+            batch_size: int = 8,
+            num_workers: int = 0,
+            pin_memory: bool = False,
+            seed: int = 42,
+    ) -> None:
+        """
+        """
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        self.data_train: Dataset | None = None
+        self.data_val: Dataset | None = None
+        self.data_test: Dataset | None = None
+
+    def setup(self, stage: str | None = None) -> None:
+        """
+        """
+        # load and split datasets only if not loaded already
+        if not self.data_train and not self.data_val and not self.data_test:
+
+            data_root = Path(self.hparams.data_root)
+
+            # get all files
+            files = [f.name for f in (data_root / 'training' / 'disp_occ_0').glob('*.png')]
+
+            # split files to train, val
+            train_size, val_size = self.hparams.train_val_split
+            train_files, val_files = train_test_split(
+                files, test_size=val_size, random_state=self.hparams.seed)
+
+            # Datasets
+            self.data_train = KITTI2015Dataset(
+                data_root=data_root / 'training',
+                file_list=train_files,
+                transforms=self.hparams.train_transforms,
+            )
+
+            self.data_val = KITTI2015Dataset(
+                data_root=data_root / 'training',
+                file_list=val_files,
+                transforms=self.hparams.val_transforms,
+            )
+
+    def train_dataloader(self) -> DataLoader:
+        """
+        Create and return the train dataloader.
+
+        Returns:
+            train_dataloader (DataLoader)
+        """
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        """
+        Create and return the validation dataloader.
+
+        Returns:
+            val_dataloader (DataLoader)
+        """
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=1,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+
+if __name__ == "__main__":
+    transforms = A.Compose(
+        [
+            A.RandomCrop(height=300, width=600),
+            A.Normalize(),
+            ToTensorV2(),
+        ],
+        additional_targets={'right': 'image', 'disp': 'mask'}
+    )
+
+    data_root = Path('datasets/KITTI2015/training')
+    files = [f.name for f in data_root.glob('image_2/*.png')]
+    dataset = KITTI2015Dataset(
+        data_root=data_root,
+        file_list=files,
+        transforms=transforms,
+    )
+
+    result = dataset[0]
+    disp = (result['disp'] / 192 * 255).cpu().numpy().astype(np.uint8)
+    left, right = result['left'], result['right']
+
+    color_disp = cv2.applyColorMap(disp, cv2.COLORMAP_JET)
+
+    x = 0
