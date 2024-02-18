334f6639672ac04701cfa508d78233bcb6aa3b31
BigDen <mangazeev.di@phystech.edu>
2024-02-18 22:34:00 +0300

    update: metrics module

--- stereodepth/metrics/__init__.py
+++ stereodepth/metrics/__init__.py
@@ -1,7 +1,5 @@
-from stereodepth.metrics.rate import RateMetric
 from stereodepth.metrics.epe import EPEMetric
 
 __all__ = [
     'EPEMetric',
-    'RateMetric',
 ]
--- stereodepth/metrics/epe.py
+++ stereodepth/metrics/epe.py
@@ -5,11 +5,16 @@ from torchmetrics import MeanMetric
 class EPEMetric(MeanMetric):
     """
     Implementations of EndPoint Error
-    """
-    def __init__(self):
-        super().__init__()
 
-    def update(self, pred: torch.Tensor, target: torch.Tensor, mask: torch.Tensor) -> None:
+    Attributes:
+        max_disp (float): maximum value of disparity in disparity mask, for KITTI2015 dataset this value is 192
+    """
+    def __init__(self, max_disp: float = 192):
+        super().__init__()
+        self.max_disp = max_disp
+
+    def update(self, pred: torch.Tensor, target: torch.Tensor) -> None:
+        mask = (pred < self.max_disp) & (pred > 1e-3)
         error = torch.abs(target - pred) * mask
         error = torch.flatten(error, 1).sum(-1)
         count = torch.flatten(mask, 1).sum(-1)
