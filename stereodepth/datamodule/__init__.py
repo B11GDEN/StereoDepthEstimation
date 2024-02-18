960944fbeaa75063b66a116d5b496afbcde75ba8
BigDen <mangazeev.di@phystech.edu>
2024-02-18 22:35:50 +0300

    add: MiddleBury datamodule

--- stereodepth/datamodule/__init__.py
+++ stereodepth/datamodule/__init__.py
@@ -1,5 +1,7 @@
 from stereodepth.datamodule.kitti2015 import KITTI2015DataModule
+from stereodepth.datamodule.middlebury2014 import Middlebury2014DataModule
 
 __all__ = [
     'KITTI2015DataModule',
+    'Middlebury2014DataModule',
 ]
