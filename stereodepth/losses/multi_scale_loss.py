f3668b57e161217830e56e76c669d46e4d4d125c
BigDen <mangazeev.di@phystech.edu>
2024-02-18 21:27:51 +0300

    add: docstring for MultiScaleLoss

--- stereodepth/losses/multi_scale_loss.py
+++ stereodepth/losses/multi_scale_loss.py
@@ -3,7 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def robust_loss(x, a, c):
+def robust_loss(x: torch.Tensor, a: float, c: float) -> torch.Tensor:
+    """
+    Robust Loss Function. From paper: A General and Adaptive Robust Loss Function
+
+    Args:
+        x (torch.Tensor): input Tensor
+        a (float): shape parameter that controls the robustness of the loss
+        c (float): a scale parameter that controls the size of the loss’s quadratic
+            bowl near x = 0.
+    """
     abs_a_sub_2 = abs(a - 2)
 
     x = x / c
@@ -15,36 +24,33 @@ def robust_loss(x, a, c):
 
 
 class MultiScaleLoss(nn.Module):
+    """
+    Multi Scale Loss Function for Stereo-Depth Estimation Task
+
+    Attributes:
+        max_disp (float): maximum value of disparity in disparity mask, for KITTI2015 dataset this value is 192
+        a (float): shape parameter of robust loss
+        c (float): scale parameter of robust loss
+    """
     def __init__(
             self,
             max_disp: float = 192,
             a: float = 0.8,
             c: float = 0.5,
-            A: float = 1,
-            tile_size: int = 1,
     ) -> None:
         super().__init__()
         self.max_disp = max_disp
         self.a = a
         self.c = c
-        self.A = A
-        self.tile_size = tile_size
 
     def forward(self, preds: dict[str, torch.Tensor], disp: torch.Tensor) -> dict[str, torch.Tensor]:
         losses = {}
         for idx, pred in enumerate(preds['multi_scale']):
             scale = disp.size(3) // pred.size(3)
-            scale_disp = max(1, scale // self.tile_size)
-
-            disp = disp / scale_disp
-            max_disp = self.max_disp / scale_disp
 
             disp = F.max_pool2d(disp, kernel_size=scale, stride=scale)
-            mask = (disp < max_disp) & (disp > 1e-3)
+            mask = (disp < self.max_disp) & (disp > 1e-3)
             diff = (pred - disp).abs()
-
-            if self.tile_size > 1 and scale_disp > 1:
-                mask = (diff < self.A) & mask
 
             loss = robust_loss(diff, a=self.a, c=self.c)
             loss = (loss * mask).sum() / (mask.sum() + 1e-6)
